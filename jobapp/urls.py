d80d2d1467c14e0a3ca21233dface13732b3ddda
Anirudh r k <menonani7@gmail.com>
2023-10-22 15:31:01 +0530

    complated the register function and added to db also

--- jobapp/urls.py
+++ jobapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns=[
     path('',views.home,name='home'),
     # path('login/',views.login_users,name='login'),
-    path('logout/',views.logout_users,name='logout')
+    path('logout/',views.logout_users,name='logout'),
+    path('register/',views.register_user,name='register')
 ]
